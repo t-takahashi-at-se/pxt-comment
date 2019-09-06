329db15d94fd9f4a26eb343b3afbb64d4be17daf
SYS-EVO\takahashi.tomoyuki <takahashi.tomoyuki@sys-evo.co.jp>
2019-09-06 17:59:08 +0900

    コメント変更

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% weight=70 icon="\uf075" color=#555555 block="コメント"
 namespace comment {
-    //% blockId=show_strings block="コメント %v"
+    //% blockId=show_strings block="髙橋コメント %v"
     export function noaction(text: string): void {
     }
 }
